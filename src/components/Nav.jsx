def6ebe3072c7d0eb88bc6a305d5827f9635e951
Aegox <diegoamundaray2017@gmail.com>
2023-04-05 22:45:11 -0500

    Finish Responsive Basic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,9 +19,11 @@ export default function Nav () {
 			return (
 			<div className={styleNav.nav}> 
 				<img src="https://1000marcas.net/wp-content/uploads/2022/04/Rick-and-Morty-500x281.png" alt="" width="280px"/>
+                <div>
 				<input type="search" placeholder="1 - 826" onChange={setValue}/>
 				<button onClick={handleChange} className={styleNav.addButton}>Add</button>
-			</div>
+			    </div>
+            </div>
 	)
 }
 
